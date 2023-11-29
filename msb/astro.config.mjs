2aa16e6a90127c98386f6ab0d10302a91036f5aa
facundolcolavini <fcolavini@southernhorizon.tech>
2023-11-29 14:29:01 -0300

    Add new components and dependencies

--- msb/astro.config.mjs
+++ msb/astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  prefetch: true,
-  integrations: [tailwind(), preact()],
-  output: 'server'
+  integrations: [tailwind(), preact({ compat: true })],
+  output: 'server',
+  adapter: netlify()
 });
